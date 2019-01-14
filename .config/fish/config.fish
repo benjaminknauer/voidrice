8c7c269b17c6845037624fb9a87afda7db23a7ef
Benjamin Knauer <knauer@creios.net>
2019-01-14 14:21:53 +0100

    added fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,34 @@
+#!/bin/bash
+#[ -f "$HOME/.shortcuts" ];  source "$HOME/.shortcuts" # Load shortcut aliases
+
+# System Maintainence
+alias mw="~/.config/mutt/mutt-wizard.sh"
+alias sdn="sudo shutdown now"
+alias psref="gpg-connect-agent RELOADAGENT /bye" # Refresh gpg
+alias gua="git remote | xargs -L1 git push --all"
+
+# Some aliases
+alias e="$EDITOR"
+alias SS="sudo systemctl"
+alias v="vim"
+alias f="vifm"
+alias r="ranger"
+alias sr="sudo ranger"
+alias ka="killall"
+alias g="git"
+alias trem="transmission-remote"
+alias mkd="mkdir -pv"
+alias ref="shortcuts >/dev/null ; source ~/.bashrc" # Refresh shortcuts manually and reload bashrc
+alias mpv="mpv --input-ipc-server=/tmp/mpvsoc(date +%s)"
+alias x="sxiv -ft *"
+
+# Adding color
+alias ls='ls -hN --color=auto --group-directories-first'
+alias grep="grep --color=auto"
+alias diff="diff --color=auto"
+alias ccat="highlight --out-format=ansi" # Color cat - print file with syntax highlighting.
+
+# Internet
+alias yt="youtube-dl --add-metadata -i" # Download video link
+alias yta="yt -x -f bestaudio/best" # Download only audio
+alias YT="youtube-viewer"
